4ae28dc5e478580c6eaab025f08bb1cd013fc942
Ezequiel Ribeiro Brilhante <ezequielrb7@gmail.com>
2019-12-07 14:15:32 -0300

    Update challenge-07.js

--- curso-javascript-ninja/challenge-07/challenge-07.js
+++ curso-javascript-ninja/challenge-07/challenge-07.js
@@ -24,31 +24,29 @@ Mostre no console o segundo elemento desse último array, criado acima, com a
 frase:
 "O segundo elemento do segundo array é [ELEMENTO]."
 */
-// ?
+console.log("O segundo elemento do segundo array é "+arr[1]);
 
 /*
 Mostre no console quantos itens tem o primeiro array criado, com a frase:
 "O primeiro array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log("O primeiro array tem "+arr.length+ " itens. ");
 
 /*
 Agora mostre no console quantos itens tem o segundo array criado, com a frase:
 "O segundo array tem [QUANTIDADE DE ITENS] itens."
 */
-// ?
+console.log("O segundo array tem "+arr.length+ " itens. ");
 
 /*
 Utilizando a estrutura de repetição `while`, mostre no console todos os números
 pares entre 10 e 20, inclusive esses 2.
 */
-console.log( 'Números pares entre 10 e 20:' );
 // ?
 
 /*
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
 */
-console.log( 'Números ímpares entre 10 e 20:' );
 // ?
 
 /*
@@ -57,8 +55,6 @@ Só vamos mudar o range:
 - No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
 */
-console.log( 'Números pares entre 100 e 120:' );
 // ?
 
-console.log( 'Números ímpares entre 111 e 125:' );
 // ?
